15eb628bf653989bb6f996259fce4a6953029233
proenca <proenca2209@gmail.com>
2026-03-30 16:10:32 -0300

    organizando o backend

--- backend/src/main/java/com/projprog/controleos/ControleosApplication.java
+++ backend/src/main/java/com/projprog/controleos/ControleosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControleosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControleosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ControleosApplication.class, args);
+    }
 
 }
